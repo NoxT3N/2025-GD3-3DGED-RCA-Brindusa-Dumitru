1d9fe63b814e9fbbad586d1ee1d73e96ed61c437
Felix <145358483+NoxT3N@users.noreply.github.com>
2025-08-16 23:49:36 +0300

    Added crop prefabs and sprites
    +a test scriptable object

--- Assets/My Assets/Scripts/CropData.cs
+++ Assets/My Assets/Scripts/CropData.cs
@@ -3,5 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CropData", menuName = "Scriptable Objects/CropData")]
 public class CropData : ScriptableObject
 {
-    
+    public string cropName;
+    public string description;
+    public Sprite icon;
+    public GameObject[] growthStages;
+    public float growthTimePerStage; //in seconds
+    public int yieldAmount; //how many items harvested
 }
